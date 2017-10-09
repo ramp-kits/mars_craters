51eac9ec2a328fd7f9ddaa503d14e4a28cb168f8
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-10-09 15:49:17 +0200

    [MRG] Cont'd image processing (#20)
    * Starting kit with image processing approach (#16)
    
    * add note book
    
    * Add the extractor just for fun
    
    * iter
    
    * iter on classifier
    
    * stupid bug
    
    * correct the output
    
    * iter
    
    * iter
    
    * iter
    
    * iter
    
    * Add Py2 specific error to flake8 ignore
    
    * FIX move everythin into single file
    
    * DOC add extractor documentation
    
    * DOC estimator documentation
    
    * non parallel version
    
    * Best optimized parameters
    
    * Remove joblib parallel
    
    * remove segmentation notebook

--- submissions/image_processing/object_detector.py
+++ submissions/image_processing/object_detector.py
@@ -1,24 +1,177 @@
+from __future__ import division
+
+import math
+from itertools import repeat
+
 import numpy as np
 
-from joblib import Parallel, delayed
+from sklearn.base import clone, BaseEstimator
+from sklearn.ensemble import GradientBoostingClassifier
 
-from sklearn.base import clone
-from imblearn.ensemble import BalancedBaggingClassifier
+from skimage.feature import blob_doh
+from mahotas.features import zernike_moments
+from mahotas.features import surf
 
-from .extraction import BlobExtractor
+###############################################################################
+# Extractor
+
+
+class BlobExtractor(BaseEstimator):
+    """Feature extractor using a blob detector.
+
+    This extractor will detect candidate regions using a blob detector,
+    i.e. maximum of the determinant of Hessian, and will extract the Zernike's
+    moments and SURF descriptors for each regions.
+
+    Parameters
+    ----------
+    min_radius : int, default=5
+        The minimum radius of the candidate to be detected.
+
+    max_radius : int, default=30
+        The maximum radius of the candidate to be detected.
+
+    blob_threshold : float, default=0.01
+        The threshold used to extract the candidate region in the DoH map.
+        Values above this threshold will be considered as a ROI.
+
+    overlap : float, default=0.2
+        A value between 0 and 1. If the area of two blobs overlaps by a
+        fraction greater than threshold, the smaller blob is eliminated.
+
+    padding : float, default=2.0
+        The region around the blob will be enlarged by the factor given in
+        padding.
+
+    iou_threshold : float, default=0.4
+        A value between 0 and 1. If the IOU between the candidate and the
+        target is greater than this threshold, the candidate is considered as a
+        crater.
+
+    """
+
+    def __init__(self, min_radius=5, max_radius=30, blob_threshold=0.01,
+                 overlap=0.2, padding=2.0, iou_threshold=0.4):
+        self.min_radius = min_radius
+        self.max_radius = max_radius
+        self.blob_threshold = blob_threshold
+        self.overlap = overlap
+        self.padding = padding
+        self.iou_threshold = iou_threshold
+
+    def fit(self, X, y=None, **fit_params):
+        # This extractor does not require any fitting
+        return self
+
+    def _extract_features(self, X, candidate):
+
+        y, x, radius = int(candidate[0]), int(candidate[1]), candidate[2]
+        padded_radius = int(self.padding * radius)
+
+        # compute the coordinate of the patch to select
+        x_min = x - padded_radius
+        x_min = x_min if x_min < 0 else 0
+        y_min = y - padded_radius
+        y_min = y_min if y_min < 0 else 0
+        x_max = x + padded_radius
+        x_max = x_max if x_max > X.shape[0] else X.shape[0] - 1
+        y_max = y + padded_radius
+        y_max = y_max if y_max > X.shape[1] else X.shape[1] - 1
+
+        patch = X[y_min:y_max, x_min:x_max]
+
+        # compute Zernike moments
+        zernike = zernike_moments(patch, radius)
+
+        # compute SURF descriptor
+        keypoint = np.array([[y, x, 1, 0.1, 1]])
+        surf_descriptor = surf.descriptors(patch, keypoint,
+                                           is_integral=False).ravel()
+        if not surf_descriptor.size:
+            surf_descriptor = np.zeros((70, ))
+
+        return np.hstack((zernike, surf_descriptor))
+
+    def extract(self, X, y=None, **fit_params):
+        candidate_blobs = blob_doh(X, min_sigma=self.min_radius,
+                                   max_sigma=self.max_radius,
+                                   threshold=self.blob_threshold,
+                                   overlap=self.overlap)
+
+        # convert the candidate to list of tuple
+        candidate_blobs = [tuple(blob) for blob in candidate_blobs]
+
+        # extract feature to be returned
+        features = [self._extract_features(X, blob)
+                    for blob in candidate_blobs]
+
+        if y is None:
+            # branch used during testing
+            return features, candidate_blobs, [None] * len(features)
+        elif not y:
+            # branch if there is no crater in the image
+            labels = [0] * len(candidate_blobs)
+
+            return features, candidate_blobs, labels
+        else:
+            # case the we did not detect any blobs
+            if not len(features):
+                return ([], [], [])
+
+            # find the maximum scores between each candidate and the
+            # ground-truth
+            scores_candidates = [max(map(cc_iou, repeat(blob, len(y)), y))
+                                 for blob in candidate_blobs]
+
+            # threshold the scores
+            labels = [0 if score < self.iou_threshold else 1
+                      for score in scores_candidates]
+
+            return features, candidate_blobs, labels
+
+    def fit_extract(self, X, y=None, **fit_params):
+        if y is None:
+            return self.fit(X, **fit_params).extract(X)
+        else:
+            return self.fit(X, y, **fit_params).extract(X, y)
+
+
+###############################################################################
+# Detector
 
 
 class ObjectDetector(object):
-    def __init__(self, extractor=None, estimator=None, n_jobs=1):
+    """Object detector.
+
+    Object detector using an extractor (which is used to extract feature) and
+    an estimator.
+
+    Parameters
+    ----------
+    extractor : object, default=BlobDetector()
+        The feature extractor used before to train the estimator.
+
+    estimator : object, default=GradientBoostingClassifier()
+        The estimator used to decide if a candidate is a crater or not.
+
+    Attributes
+    ----------
+    extractor_ : object,
+        The actual extractor used after fit.
+
+    estimator_ : object,
+        The actual estimator used after fit.
+
+    """
+
+    def __init__(self, extractor=None, estimator=None):
         self.extractor = extractor
         self.estimator = estimator
-        self.n_jobs = n_jobs
 
     def _extract_features(self, X, y):
         # extract feature for all the image containing craters
-        data_extracted = Parallel(n_jobs=self.n_jobs)(
-            delayed(self.extractor_.fit_extract)(image, craters)
-            for image, craters in zip(X, y))
+        data_extracted = [self.extractor_.fit_extract(image, craters)
+                          for image, craters in zip(X, y)]
 
         # organize the data to fit it inside the classifier
         data, location, target, idx_cand_to_img = [], [], [], []
@@ -42,7 +195,7 @@ class ObjectDetector(object):
             self.extractor_ = clone(self.extractor)
 
         if self.estimator is None:
-            self.estimator_ = BalancedBaggingClassifier(n_jobs=self.n_jobs)
+            self.estimator_ = GradientBoostingClassifier(n_estimators=100)
         else:
             self.estimator_ = clone(self.estimator)
 
@@ -70,3 +223,82 @@ class ObjectDetector(object):
                                     pred[crater_idx]))
 
         return output
+
+###############################################################################
+# IOU function
+
+
+def cc_iou(circle1, circle2):
+    """
+    Intersection over Union (IoU) between two circles
+
+    Parameters
+    ----------
+    circle1 : tuple of floats
+        first circle parameters (x_pos, y_pos, radius)
+    circle2 : tuple of floats
+        second circle parameters (x_pos, y_pos, radius)
+
+    Returns
+    -------
+    float
+        ratio between area of intersection and area of union
+
+    """
+    x1, y1, r1 = circle1
+    x2, y2, r2 = circle2
+
+    d = math.hypot(x2 - x1, y2 - y1)
+
+    area_intersection = cc_intersection(d, r1, r2)
+    area_union = math.pi * (r1 * r1 + r2 * r2) - area_intersection
+
+    return area_intersection / area_union
+
+
+def cc_intersection(dist, rad1, rad2):
+    """
+    Area of intersection between two circles
+
+    Parameters
+    ----------
+    dist : positive float
+        distance between circle centers
+    rad1 : positive float
+        radius of first circle
+    rad2 : positive float
+        radius of second circle
+
+    Returns
+    -------
+    intersection_area : positive float
+        area of intersection between circles
+
+    References
+    ----------
+    http://mathworld.wolfram.com/Circle-CircleIntersection.html
+
+    """
+    if dist < 0:
+        raise ValueError("Distance between circles must be positive")
+    if rad1 < 0 or rad2 < 0:
+        raise ValueError("Circle radius must be positive")
+
+    if dist == 0 or (dist <= abs(rad2 - rad1)):
+        return min(rad1, rad2) ** 2 * math.pi
+
+    if dist > rad1 + rad2 or rad1 == 0 or rad2 == 0:
+        return 0
+
+    rad1_sq = rad1 * rad1
+    rad2_sq = rad2 * rad2
+
+    circle1 = rad1_sq * math.acos((dist * dist + rad1_sq - rad2_sq) /
+                                  (2 * dist * rad1))
+    circle2 = rad2_sq * math.acos((dist * dist + rad2_sq - rad1_sq) /
+                                  (2 * dist * rad2))
+    intersec = 0.5 * math.sqrt((-dist + rad1 + rad2) * (dist + rad1 - rad2) *
+                               (dist - rad1 + rad2) * (dist + rad1 + rad2))
+    intersection_area = circle1 + circle2 + intersec
+
+    return intersection_area
