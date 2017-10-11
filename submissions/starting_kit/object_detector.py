93455267371fd772ecbef591cdd7b18900a1434e
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2017-10-11 15:22:17 +0200

    Starting kit submission: make sure to return array of lists (#34)

--- submissions/starting_kit/object_detector.py
+++ submissions/starting_kit/object_detector.py
@@ -9,4 +9,7 @@ class ObjectDetector:
         return self
 
     def predict(self, X):
-        return np.array([[(1.0, 112.0, 112.0, 112.0)] for img in X])
+        y_pred = [[(1.0, 112.0, 112.0, 112.0)] for img in X]
+        y_pred_array = np.empty(len(y_pred), dtype=object)
+        y_pred_array[:] = y_pred
+        return y_pred
